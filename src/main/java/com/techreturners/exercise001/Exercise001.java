7d25af02dcdd9643eac1a88b66b923863cb8a75d
Ellie Judge <judge_ellie.com>
2020-11-22 17:44:52 +0000

    Completed exercises 001

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,30 +1,34 @@
 package com.techreturners.exercise001;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
+        return firstName.substring(0, 1).toUpperCase() + "." + lastName.substring(0, 1).toUpperCase();
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+        double vat = (originalPrice / 100) * vatRate;
+        DecimalFormat df = new DecimalFormat("#.00");
+        return Double.parseDouble(df.format(vat + originalPrice));
     }
 
     public String reverse(String sentence) {
-        // Add your code here
-        return "";
+        return new StringBuilder(sentence).reverse().toString();
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+        int count = 0;
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getType().equals("Linux")) {
+                count++;
+            }
+        };
+        return count;
     }
 }
